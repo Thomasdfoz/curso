c8759ca6203651c3f34551dff56f84b3e355501b
Thomas Barros <thomas_sp18@hotmail.com>
2019-02-01 20:11:20 -0200

    projeto iniciado, mostrando a mensagem hello world

---
+++ Projeto_test/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Projeto_test {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
