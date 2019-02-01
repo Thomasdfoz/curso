efbfa0182e757b52f00bb1b1f738268e1ec74199
Thomas Barros <thomas_sp18@hotmail.com>
2019-02-01 20:13:55 -0200

    Acrescentada uma mensagem de good morning

--- Projeto_test/Program.cs
+++ Projeto_test/Program.cs
@@ -4,6 +4,7 @@ namespace Projeto_test {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Good Morning");
             Console.ReadLine();
         }
     }
